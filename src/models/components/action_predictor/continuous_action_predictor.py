18cb4b6726b53fa717d4b0036c95ed902d896169
myxyy <m.myxomatosis@gmail.com>
2023-08-26 13:45:37 +0900

    連続行動空間向けに修正

--- src/models/components/action_predictor/continuous_action_predictor.py
+++ src/models/components/action_predictor/continuous_action_predictor.py
@@ -3,19 +3,18 @@ import torch
 import torch.nn as nn
 from torch import Tensor
 
-class DiscreteActionPredictor(ActionPredictor):
+class ContinuousActionPredictor(ActionPredictor):
     def __init__(self, dim_embed: int, dim_action: int):
         super().__init__()
         self.linear_1 = nn.Linear(dim_embed*2, dim_embed*2+dim_action)
         self.linear_2 = nn.Linear(dim_embed*2+dim_action, dim_action)
         self.relu = nn.ReLU()
-        self.softmax = nn.Softmax()
+        self.mse_loss = nn.MSELoss()
 
     def forward(self, embed: Tensor, next_embed: Tensor) -> Tensor:
         x = torch.concat([embed, next_embed], dim=-1)
         x = self.linear_1(x)
         x = self.relu(x)
-        x = self.linear_2(x)
-        x = self.softmax(x)
-        return x
+        action_hat = self.linear_2(x)
+        return action_hat
 
