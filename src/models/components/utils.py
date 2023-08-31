7f7abed66b6ce4de97d9c4931ddb5e111760aa4e
myxyy <m.myxomatosis@gmail.com>
2023-08-31 20:54:08 +0900

    階層の変更とモデルのDI

--- src/models/components/utils.py
+++ src/models/components/utils.py
@@ -1,17 +1,10 @@
 import torch
-import torch.distributions as distributions
 import torch.nn as nn
-from torch import Tensor
-
-from ..policy.stochastic_policy import StochasticPolicy
-from ..value.value import Value
 
 
-class PolicyValueCommonNet(nn.Module):
-    def __init__(self, policy: StochasticPolicy, value: Value, height: int, width: int, dim_hidden: int):
+class SmallConvNet(nn.Module):
+    def __init__(self, height: int, width: int, dim_out: int):
         super().__init__()
-        self.policy = policy
-        self.value = value
         self.conv2d1 = nn.Conv2d(3, 32, 8, stride=4)
         self.conv2d2 = nn.Conv2d(32, 64, 4, stride=2)
         self.conv2d3 = nn.Conv2d(64, 64, 3, stride=1)
@@ -19,13 +12,17 @@ class PolicyValueCommonNet(nn.Module):
             ((((height - (8 - 4)) // 4 - (4 - 2)) // 2 - (3 - 1)) // 1)
             * ((((width - (8 - 4)) // 4 - (4 - 2)) // 2 - (3 - 1)) // 1)
             * 64,
-            dim_hidden,
+            dim_out,
         )
+        self.relu = nn.ReLU()
 
     def forward(self, x):
         x = self.conv2d1(x)
+        x = self.relu(x)
         x = self.conv2d2(x)
+        x = self.relu(x)
         x = self.conv2d3(x)
+        x = self.relu(x)
         x = x.view(x.size()[0], -1)
         x = self.fc(x)
-        return self.policy(x), self.value(x)
+        return x
--- tests/models/components/test_policy_value_common_net.py
+++ tests/models/components/test_policy_value_common_net.py
@@ -3,9 +3,8 @@ import torch
 from torch.distributions.distribution import Distribution
 
 from src.models.components.policy.normal_stochastic_policy import NormalStochasticPolicy
-from src.models.components.policy_value_common_net.policy_value_common_net import (
-    PolicyValueCommonNet,
-)
+from src.models.components.policy_value_common_net import PolicyValueCommonNet
+from src.models.components.utils import SmallConvNet
 from src.models.components.value.fully_connect_value import FullyConnectValue
 
 
@@ -26,7 +25,8 @@ from src.models.components.value.fully_connect_value import FullyConnectValue
 def test_policy_value_common_net(batch, height, width, dim_hidden, dim_dist):
     policy = NormalStochasticPolicy(dim_hidden, dim_dist)
     value = FullyConnectValue(dim_hidden)
-    pvc = PolicyValueCommonNet(policy, value, height, width, dim_hidden)
+    base_model = SmallConvNet(height, width, dim_hidden)
+    pvc = PolicyValueCommonNet(base_model, policy, value, height, width, dim_hidden)
     input = torch.randn(batch, 3, height, width)
     dist, value = pvc(input)
     assert dist.rsample().size() == (batch, dim_dist)
