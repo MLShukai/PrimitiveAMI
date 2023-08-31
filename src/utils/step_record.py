4453f49c434721c58fc8c63a8ad5a0c5e10124c6
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2023-08-31 13:53:13 +0900

    ADD RecordKeys enum.

--- src/utils/step_record.py
+++ src/utils/step_record.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import copy
 from collections import UserDict
+from enum import Enum
 
 
 class StepRecord(UserDict):
@@ -14,3 +15,16 @@ class StepRecord(UserDict):
             self (Self): deep copied data.
         """
         return copy.deepcopy(self)
+
+
+class RecordKeys(str, Enum):
+    PREVIOUS_ACTION = "previous_action"  # a_{t-1}
+    OBSERVATION = "observation"  # o_t
+    EMBED_OBSERVATION = "embed_observation"  # z_t
+    ACTION = "action"  # a_t
+    ACTION_LOG_PROBABILITY = "action_log_probability"
+    VALUE = "value"  # v_t
+    PREDICTED_NEXT_EMBED_OBSERVATION = "predicted_next_embed_observation"  # \hat{z}_{t+1}
+    NEXT_OBSERVATION = "next_observation"  # o_{t+1}
+    NEXT_EMBED_OBSERVATION = "next_embed_observation"  # z_{t+1}
+    REWARD = "reward"  # r_{t+1}
--- tests/utils/test_step_record.py
+++ tests/utils/test_step_record.py
@@ -1,6 +1,8 @@
+from enum import Enum
+
 import pytest
 
-from src.utils.step_record import StepRecord
+from src.utils.step_record import RecordKeys, StepRecord
 
 
 class TestStepRecord:
@@ -12,3 +14,20 @@ class TestStepRecord:
         other = step_record.copy()
         assert other == step_record
         assert other is not step_record
+
+
+class TestRecordKeys:
+    def test_enum(self):
+        assert issubclass(RecordKeys, (str, Enum))
+
+    def test_enum_values(self):
+        assert RecordKeys.PREVIOUS_ACTION == "previous_action"
+        assert RecordKeys.OBSERVATION == "observation"
+        assert RecordKeys.EMBED_OBSERVATION == "embed_observation"
+        assert RecordKeys.ACTION == "action"
+        assert RecordKeys.ACTION_LOG_PROBABILITY == "action_log_probability"
+        assert RecordKeys.VALUE == "value"
+        assert RecordKeys.PREDICTED_NEXT_EMBED_OBSERVATION == "predicted_next_embed_observation"
+        assert RecordKeys.NEXT_OBSERVATION == "next_observation"
+        assert RecordKeys.NEXT_EMBED_OBSERVATION == "next_embed_observation"
+        assert RecordKeys.REWARD == "reward"
