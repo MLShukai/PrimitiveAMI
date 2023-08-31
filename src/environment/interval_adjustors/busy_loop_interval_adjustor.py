16dff0391ab553e29624bc1c18eede304b6a379a
cehl-kurage <yusakuishihira@gmail.com>
2023-08-31 14:56:55 +0900

    adjustメソッドを修正

--- src/environment/interval_adjustors/busy_loop_interval_adjustor.py
+++ src/environment/interval_adjustors/busy_loop_interval_adjustor.py
@@ -1,14 +1,16 @@
-from .interval_adjustor import IntervalAdjustor
 import time
+
+from .interval_adjustor import IntervalAdjustor
 
 
 class BusyLoopIntervalAdjustor(IntervalAdjustor):
-    def __init__(self, interval: float) -> None:
-        super().__init__(interval)
+    def __init__(self, interval: float, offset: float = 0.0) -> None:
+        super().__init__(interval, offset)
 
-    def adjust(self, offset: float = 0.0) -> float:
-        time_to_sleep = self.interval - offset
-        start_time = self.reset()
-        while start_time + time_to_sleep >= time.perf_counter():
+    def adjust(self) -> float:
+        interval, offset = self.interval, self.offset
+        time_to_sleep = interval - offset
+        while self._start_time + time_to_sleep > time.perf_counter():
             pass
+        self.reset()
         return time_to_sleep
