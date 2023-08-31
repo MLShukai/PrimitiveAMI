37c37683fcf8f5b7a8f5202f493a20a5287c66e9
myxyy <m.myxomatosis@gmail.com>
2023-08-31 21:28:19 +0900

    TanhNormal追加

---
+++ src/models/components/policy/tahh_normal_stochastic_policy.py
@@ -0,0 +1,22 @@
+import torch
+import torch.distributions as distributions
+import torch.nn as nn
+import torchrl
+from torch import Tensor
+from torch.distributions.distribution import Distribution
+
+from .stochastic_policy import StochasticPolicy
+
+
+class TanhNormalStochasticPolicy(StochasticPolicy):
+    def __init__(self, dim_input: int, dim_out: int):
+        super().__init__()
+        self.fc_mean = nn.Linear(dim_input, dim_out)
+        self.fc_std = nn.Linear(dim_input, dim_out)
+        self.softplus = nn.Softplus()
+
+    def forward(self, input: Tensor) -> Distribution:
+        mean = self.fc_mean(input)
+        std = self.softplus(self.fc_std(input)) + 1e-7  # std 0 causes error
+        tanh_norm_dist = torchrl.modules.TanhNormal(mean, std)
+        return tanh_norm_dist
