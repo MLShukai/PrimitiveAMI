80335a5d046c312cbed86f1ddac2f33dc38c8125
cehl-kurage <yusakuishihira@gmail.com>
2023-09-01 14:44:10 +0900

    _time_to_sleepを利用する実装に変更

--- src/environment/interval_adjustors/sleep_interval_adjustor.py
+++ src/environment/interval_adjustors/sleep_interval_adjustor.py
@@ -4,9 +4,6 @@ from .interval_adjustor import IntervalAdjustor
 
 
 class SleepIntervalAdjustor(IntervalAdjustor):
-    def __init__(self, interval: float, offset: float = 0.0) -> None:
-        super().__init__(interval, offset)
-
     def adjust(self) -> float:
         """Adjust time by time.sleep.
 
@@ -16,9 +13,8 @@ class SleepIntervalAdjustor(IntervalAdjustor):
         Returns:
             float: Ideal value of time waited.
         """
-        interval, offset = self.interval, self.offset
-        time_to_sleep = interval - offset
-        if (remaining_time := (self._start_time + time_to_sleep) - time.perf_counter()) > 0:
+        if (remaining_time := (self._start_time + self._time_to_sleep) - time.perf_counter()) > 0:
             time.sleep(remaining_time)
+        delta_time = time.perf_counter() - self._start_time
         self.reset()
-        return time_to_sleep
+        return delta_time
