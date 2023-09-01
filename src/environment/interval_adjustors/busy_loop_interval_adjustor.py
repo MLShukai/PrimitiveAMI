aae7258e6e1c55556d025b728e045d70691ca17e
cehl-kurage <yusakuishihira@gmail.com>
2023-09-01 14:44:58 +0900

    _time_to_sleepを利用する実装に変更

--- src/environment/interval_adjustors/busy_loop_interval_adjustor.py
+++ src/environment/interval_adjustors/busy_loop_interval_adjustor.py
@@ -4,13 +4,11 @@ from .interval_adjustor import IntervalAdjustor
 
 
 class BusyLoopIntervalAdjustor(IntervalAdjustor):
-    def __init__(self, interval: float, offset: float = 0.0) -> None:
-        super().__init__(interval, offset)
-
     def adjust(self) -> float:
-        interval, offset = self.interval, self.offset
-        time_to_sleep = interval - offset
-        while self._start_time + time_to_sleep > time.perf_counter():
+        end_time = self._start_time + self._time_to_sleep
+
+        while end_time > time.perf_counter():
             pass
+        delta_time = time.perf_counter() - self._start_time
         self.reset()
-        return time_to_sleep
+        return delta_time
