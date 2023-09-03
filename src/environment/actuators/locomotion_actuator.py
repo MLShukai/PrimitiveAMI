48d57a854bc786436d92d7a5f608cff407f25d8c
cehl-kurage <yusakuishihira@gmail.com>
2023-09-03 17:34:38 +0900

    型アノテーションを修正。引数の渡し方を修正。

---
+++ src/environment/actuators/locomotion_actuator.py
@@ -0,0 +1,13 @@
+import torch
+from vrchat_io.controller.wrappers.osc import AxesLocomotionWrapper
+
+from .actuator import Actuator
+
+
+class LocomotionActuator(Actuator):
+    def __init__(self, actuator: AxesLocomotionWrapper):
+        self.actuator = actuator
+
+    def operate(self, action: torch.Tensor) -> None:
+        _action = action.detach().cpu().numpy()
+        self.actuator.command(_action[0], _action[1], _action[2])
