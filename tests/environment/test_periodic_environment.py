8052b4438ffc1ec1928d44850b8515b80976a830
cehl-kurage <yusakuishihira@gmail.com>
2023-09-03 20:48:08 +0900

    PeriodicEnvironmentのテストを追加

--- tests/environment/test_periodic_environment.py
+++ tests/environment/test_periodic_environment.py
@@ -1,35 +1,62 @@
-from src.environment.periodic_environment import PeriodicEnvironment as cls
-import pytest
-from src.environment.sensors.frame_sensor import FrameSensor
-from src.environment.actuators.locomotion_actuator import LocomotionActuator
-from src.environment.interval_adjustors.sleep_interval_adjustor import SleepIntervalAdjustor
 import time
 
-adjustor_params = {"interval": 0.1, "offset": 0.}
+import pytest
+import torch
+
+from src.environment.actuators.locomotion_actuator import LocomotionActuator
+from src.environment.interval_adjustors.sleep_interval_adjustor import (
+    SleepIntervalAdjustor,
+)
+from src.environment.periodic_environment import PeriodicEnvironment as cls
+from src.environment.sensors.frame_sensor import FrameSensor
+
+adjustor_params = {"interval": 0.1, "offset": 0.0}
 
 
 class TestPeriodicEnvironment:
-
     @pytest.fixture
     def sensor(self, mocker):
         mock_sensor = mocker.Mock(spec=FrameSensor)
+        mock_sensor.read.return_value = torch.zeros((3, 640, 480))
         return mock_sensor
-    
+
     @pytest.fixture
     def actuator(self, mocker):
         mock_actuator = mocker.Mock(spec=LocomotionActuator)
         return mock_actuator
-    
+
     @pytest.fixture
-    def adjustor(self):
-        adjustor = SleepIntervalAdjustor(adjustor_params["interval"], adjustor_params["offset"])
-        return adjustor
-    
+    def adjustor(self, mocker):
+        mock_adjustor = mocker.Mock(spec=SleepIntervalAdjustor)
+        mock_adjustor.reset.return_value = 0.0
+        return mock_adjustor
+
+    @pytest.fixture
+    def mod(self, sensor, actuator, adjustor):
+        return cls(sensor, actuator, adjustor)
+
     def test__init__(self, sensor, actuator, adjustor):
         mod = cls(sensor, actuator, adjustor)
         assert mod.sensor is sensor
         assert mod.actuator is actuator
         assert mod.adjustor is adjustor
 
-    def test_setup(self, mocker):
-        with mocker.patch("")
+    def test_setup(self, mocker, sensor, actuator, adjustor):
+        mod = mocker.Mock(spec=cls)
+        mod.setup()
+        assert mod.setup.call_count == 1
+
+    def test_observe(self, mod):
+        assert (mod.observe() == torch.zeros((3, 640, 480))).all()
+
+    def test_affect(self, mod):
+        action = torch.tensor([0.0, 0.0, 0.0], dtype=torch.float64)
+        mod.affect(action)
+        mod.actuator.operate.assert_called_with(action)
+
+    def test_reset_interval_start_time(self, mod):
+        assert mod.reset_interval_start_time() == 0.0
+
+    def test_adjust_interval(self, mod):
+        mod.adjust_interval()
+        assert mod.adjustor.adjust.call_count == 1
