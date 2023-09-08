52e1ed537859b066bf8aabfb78f027f65a63a1e6
myxyy <m.myxomatosis@gmail.com>
2023-09-08 22:30:26 +0900

    DynamicsDataCollector修正

--- src/data_collectors/dynamics_data_collector.py
+++ src/data_collectors/dynamics_data_collector.py
@@ -3,42 +3,36 @@ from typing import Any
 import numpy
 import torch
 from torch import Tensor
+from torch.utils.data import TensorDataset
 
 from ..utils.step_record import RecordKeys as RK
 from .data_collector import DataCollector
 
 
 class DynamicsDataCollector(DataCollector):
-    def __init__(self, decay_prob: float, max_size: int):
-        self.decay_prob = decay_prob
+    def __init__(self, max_size: int):
         self.max_size = max_size
         self.prev_actions = []
         self.observations = []
         self.actions = []
         self.next_observations = []
 
-    def collect(self, step_record: dict[str, Any]):
-        self.prev_actions = self._list_decay(self.prev_actions)[-(self.max_size - 1) :]
-        self.observations = self._list_decay(self.observations)[-(self.max_size - 1) :]
-        self.actions = self._list_decay(self.actions)[-(self.max_size - 1) :]
-        self.next_observations = self._list_decay(self.next_observations)[-(self.max_size - 1) :]
+    def collect(self, step_record: dict[str, Tensor]):
+        if len(self.prev_actions) < self.max_size:
+            self.prev_actions.append(step_record[RK.PREVIOUS_ACTION].clone().cpu())
+            self.observations.append(step_record[RK.OBSERVATION].clone().cpu())
+            self.actions.append(step_record[RK.ACTION].clone().cpu())
+            self.next_observations.append(step_record[RK.NEXT_OBSERVATION].clone().cpu())
+        else:
+            rand_ind = numpy.random.randomint(0, self.max_size)
+            self.prev_actions[rand_ind] = step_record[RK.PREVIOUS_ACTION].clone().cpu()
+            self.observations[rand_ind] = step_record[RK.OBSERVATION].clone().cpu()
+            self.actions[rand_ind] = step_record[RK.ACTION].clone().cpu()
+            self.next_observations[rand_ind] = step_record[RK.NEXT_OBSERVATION].clone().cpu()
 
-        self.prev_actions.append(step_record[RK.PREVIOUS_ACTION])
-        self.observations.append(step_record[RK.OBSERVATION])
-        self.actions.append(step_record[RK.ACTION])
-        self.next_observations.append(step_record[RK.NEXT_OBSERVATION])
-
-    def _list_decay(self, list: [Any]) -> [Any]:
-        return_list = []
-        while len(list) > 0:
-            item = list.pop(0)
-            if numpy.random.rand() > self.decay_prob:
-                return_list.append(item)
-        return return_list
-
-    def get_data(self) -> (Tensor, Tensor, Tensor, Tensor):
+    def get_data(self) -> TensorDataset:
         prev_actions = torch.stack(self.prev_actions)
         observations = torch.stack(self.observations)
         actions = torch.stack(self.actions)
         next_observations = torch.stack(self.next_observations)
-        return prev_actions, observations, actions, next_observations
+        return TensorDataset(prev_actions, observations, actions, next_observations)
--- tests/dara_collectors/test_dynamics_data_collector.py
+++ tests/dara_collectors/test_dynamics_data_collector.py
@@ -7,36 +7,35 @@ from src.utils.step_record import RecordKeys as RK
 
 @pytest.mark.parametrize(
     """
-    decay_prob,
     max_size,
-    observation_dim,
-    action_dim,
+    observation_shape,
+    action_shape,
     """,
     [
-        (0, 302, 68, 73),
-        (0.01, 400, 62, 33),
-        (0.99, 320, 64, 43),
-        (1, 176, 134, 51),
+        (302, (68,), (73, 4)),
+        (400, (62, 5), (33,)),
+        (320, (64, 45), (43, 5)),
+        (176, (134,), (51,)),
     ],
 )
-def test_dynamics_data_collector(decay_prob, max_size, observation_dim, action_dim):
-    dynamics_data_collector = DynamicsDataCollector(decay_prob, max_size)
+def test_dynamics_data_collector(max_size, observation_shape, action_shape):
+    dynamics_data_collector = DynamicsDataCollector(max_size)
     data = {
-        RK.PREVIOUS_ACTION: torch.randn(action_dim),
-        RK.OBSERVATION: torch.randn(observation_dim),
-        RK.ACTION: torch.randn(action_dim),
-        RK.NEXT_OBSERVATION: torch.randn(observation_dim),
+        RK.PREVIOUS_ACTION: torch.randn(*action_shape),
+        RK.OBSERVATION: torch.randn(*observation_shape),
+        RK.ACTION: torch.randn(*action_shape),
+        RK.NEXT_OBSERVATION: torch.randn(*observation_shape),
     }
 
     for _ in range(128):
         dynamics_data_collector.collect(data)
 
-    prev_actions, observations, actions, next_observations = dynamics_data_collector.get_data()
-    assert prev_actions.size()[0] <= max_size
-    assert prev_actions.size()[1] == action_dim
-    assert observations.size()[0] <= max_size
-    assert observations.size()[1] == observation_dim
-    assert actions.size()[0] <= max_size
-    assert actions.size()[1] == action_dim
-    assert next_observations.size()[0] <= max_size
-    assert next_observations.size()[1] == observation_dim
+    prev_actions, observations, actions, next_observations = dynamics_data_collector.get_data()[0:max_size]
+    assert prev_actions.size(0) <= max_size
+    assert prev_actions.size()[1:] == (*action_shape,)
+    assert observations.size(0) <= max_size
+    assert observations.size()[1:] == (*observation_shape,)
+    assert actions.size(0) <= max_size
+    assert actions.size()[1:] == (*action_shape,)
+    assert next_observations.size(0) <= max_size
+    assert next_observations.size()[1:] == (*observation_shape,)
