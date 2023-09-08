b821055b7741d3ca039937a0e91079b6564cf79c
myxyy <m.myxomatosis@gmail.com>
2023-09-08 22:01:21 +0900

    DummyDynamicsDataCollector修正

--- src/data_collectors/dummy_dynamics_data_collector.py
+++ src/data_collectors/dummy_dynamics_data_collector.py
@@ -2,23 +2,24 @@ from typing import Any
 
 import torch
 from torch import Tensor
+from torch.utils.data import TensorDataset
 
 from ..utils.step_record import RecordKeys as RK
 from .data_collector import DataCollector
 
 
 class DummyDynamicsDataCollector(DataCollector):
-    def __init__(self, action_dim: int, observation_dim: int, get_size: int):
-        self.action_dim = action_dim
-        self.observation_dim = observation_dim
+    def __init__(self, action_shape: tuple[int], observation_shape: tuple[int], get_size: int):
+        self.action_shape = action_shape
+        self.observation_shape = observation_shape
         self.get_size = get_size
 
     def collect(self, step_record: dict[str, Any]):
         pass
 
-    def get_data(self) -> (Tensor, Tensor, Tensor, Tensor):
-        prev_actions = torch.randn(self.get_size, self.action_dim)
-        observations = torch.randn(self.get_size, self.observation_dim)
-        actions = torch.randn(self.get_size, self.action_dim)
-        next_observations = torch.randn(self.get_size, self.observation_dim)
-        return prev_actions, observations, actions, next_observations
+    def get_data(self) -> TensorDataset:
+        prev_actions = torch.randn(self.get_size, *self.action_shape)
+        observations = torch.randn(self.get_size, *self.observation_shape)
+        actions = torch.randn(self.get_size, *self.action_shape)
+        next_observations = torch.randn(self.get_size, *self.observation_shape)
+        return TensorDataset(prev_actions, observations, actions, next_observations)
--- tests/dara_collectors/test_dummy_dynamics_data_collector.py
+++ tests/dara_collectors/test_dummy_dynamics_data_collector.py
@@ -7,21 +7,21 @@ from src.utils.step_record import RecordKeys as RK
 
 @pytest.mark.parametrize(
     """
-    action_dim,
-    observation_dim,
+    action_shape,
+    observation_shape,
     get_size,
     """,
     [
-        (4, 623, 3),
-        (32, 64, 4),
-        (17, 234, 5),
+        ((4, 1), (623,), 3),
+        ((32,), (5, 64), 4),
+        ((17, 4, 7), (1, 3, 234), 5),
     ],
 )
-def test_dummy_dynamics_data_collector(action_dim, observation_dim, get_size):
-    dummy_dynamics_data_collector = DummyDynamicsDataCollector(action_dim, observation_dim, get_size)
+def test_dummy_dynamics_data_collector(action_shape, observation_shape, get_size):
+    dummy_dynamics_data_collector = DummyDynamicsDataCollector(action_shape, observation_shape, get_size)
 
-    prev_actions, observations, actions, next_observations = dummy_dynamics_data_collector.get_data()
-    assert prev_actions.size() == (get_size, action_dim)
-    assert observations.size() == (get_size, observation_dim)
-    assert actions.size() == (get_size, action_dim)
-    assert next_observations.size() == (get_size, observation_dim)
+    prev_actions, observations, actions, next_observations = dummy_dynamics_data_collector.get_data()[0:get_size]
+    assert prev_actions.size() == (get_size, *action_shape)
+    assert observations.size() == (get_size, *observation_shape)
+    assert actions.size() == (get_size, *action_shape)
+    assert next_observations.size() == (get_size, *observation_shape)
