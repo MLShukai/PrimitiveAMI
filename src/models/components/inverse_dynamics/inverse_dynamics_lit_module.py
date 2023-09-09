252a366e6a966b57aa04045200851eb10363b08f
cehl-kurage <yusakuishihira@gmail.com>
2023-09-09 15:08:44 +0900

    InverseDynamicsLitModuleを実装

---
+++ src/models/components/inverse_dynamics/inverse_dynamics_lit_module.py
@@ -0,0 +1,28 @@
+from typing import Any
+
+from lightning import LightningModule
+from lightning.pytorch.utilities.types import STEP_OUTPUT
+from torch import Tensor
+from torch.nn.functional import mse_loss
+from torch.optim import Adam, Optimizer
+
+from .inverse_dynamics import InverseDynamics
+
+
+class InverseDynamicsLitModule(LightningModule):
+    def __init__(self, inverse_dynamics_net: InverseDynamics, optimizer: Optimizer):
+        super().__init__()
+        self.save_hyperparameters(
+            logger=False
+        )  # https://lightning.ai/docs/pytorch/1.6.2/common/hyperparameters.html#save-hyperparameters
+
+    def configure_optimizers(self) -> Optimizer:
+
+        optimizer = Adam(self.hparams.inverse_dynamics_net.parameters(), lr=1e-4)
+        return optimizer
+
+    def training_step(self, batch: Tensor, batch_idx: int):
+        prev_action, obs, action, next_obs = batch
+        prev_action_hat, action_hat = self.hparams.inverse_dynamics_net(obs, next_obs)
+        loss = mse_loss(prev_action_hat, prev_action) + mse_loss(action_hat, action)
+        return loss
