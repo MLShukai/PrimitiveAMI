ae0fecd057c9588dd49b5a0289feae71477b50f5
myxyy <m.myxomatosis@gmail.com>
2023-09-10 18:01:39 +0900

    ForwardDynamicsLitModuleの実装

---
+++ src/models/components/forward_dynamics/forward_dynamics_lit_module.py
@@ -0,0 +1,28 @@
+import torch
+from lightning import LightningModule
+from torch.nn.functional import mse_loss
+from torch.optim import Optimizer
+
+from ..observation_encoder.observation_encoder import ObservationEncoder
+from .forward_dynamics import ForwardDynamics
+
+
+class ForwardDynamicsLitModule(LightningModule):
+    def __init__(self, obs_encoder: ObservationEncoder, forward_dynamics_net: ForwardDynamics, optimizer: Optimizer):
+        super().__init__()
+        self.save_hyperparameters(logger=False)
+        self.obs_encoder = obs_encoder
+        self.forward_dynamics_net = forward_dynamics_net
+        self.optimizer = optimizer
+
+    def configure_optimizer(self) -> Optimizer:
+        return self.optimizer
+
+    def training_step(self, batch, batch_idx):
+        prev_action, obs, action, next_obs = batch
+        with torch.no_grad():
+            embed_obs = self.obs_encoder(obs)
+            embed_next_obs = self.obs_encoder(next_obs)
+        embed_next_obs_hat = self.forward_dynamics_net(prev_action, embed_obs, action)
+        loss = mse_loss(embed_next_obs_hat, embed_next_obs)
+        return loss
---
+++ tests/models/components/forward_dynamics/test_forward_dynamics_lit_module.py
@@ -0,0 +1,54 @@
+import pytest
+import torch
+from pytest_mock import MockerFixture
+from torch.optim import Adam, Optimizer
+
+from src.models.components.forward_dynamics.dense_net_forward_dynamics import (
+    DenseNetForwardDynamics,
+)
+from src.models.components.forward_dynamics.forward_dynamics import ForwardDynamics
+from src.models.components.forward_dynamics.forward_dynamics_lit_module import (
+    ForwardDynamicsLitModule,
+)
+from src.models.components.observation_encoder.observation_encoder import (
+    ObservationEncoder,
+)
+
+num_batch = 32
+obs_emb_dim = 256
+action_dim = 128
+batch = (
+    torch.randn(num_batch, action_dim),
+    torch.randn(num_batch, obs_emb_dim),
+    torch.randn(num_batch, action_dim),
+    torch.randn(num_batch, obs_emb_dim),
+)
+
+
+@pytest.fixture
+def mock_observation_encoder(mocker: MockerFixture) -> ObservationEncoder:
+    mock = mocker.Mock(spec=ObservationEncoder, return_value=torch.randn(num_batch, obs_emb_dim))
+    return mock
+
+
+@pytest.fixture
+def mock_forward_dynamics_net() -> ForwardDynamics:
+    return DenseNetForwardDynamics(action_dim, obs_emb_dim)
+
+
+@pytest.fixture
+def mock_optimizer(mock_forward_dynamics_net):
+    return Adam(mock_forward_dynamics_net.parameters())
+
+
+@pytest.fixture
+def forward_dynamics_lit_module(
+    mock_observation_encoder: ObservationEncoder,
+    mock_forward_dynamics_net: ForwardDynamics,
+    mock_optimizer: Optimizer,
+) -> ForwardDynamicsLitModule:
+    return ForwardDynamicsLitModule(mock_observation_encoder, mock_forward_dynamics_net, mock_optimizer)
+
+
+def test_forward_dynamics_lit_module(forward_dynamics_lit_module: ForwardDynamicsLitModule):
+    loss = forward_dynamics_lit_module.training_step(batch, 0)
