64e129fa8977deb624dde45f0f8a39cc65aee45c
cehl-kurage <yusakuishihira@gmail.com>
2023-09-10 20:27:26 +0900

    テストの粒度と実装を変更

--- tests/models/components/inverse_dynamics/test_inverse_dynamics_lit_module.py
+++ tests/models/components/inverse_dynamics/test_inverse_dynamics_lit_module.py
@@ -1,65 +1,55 @@
 from copy import deepcopy
+from functools import partial
 
 import pytest
 import torch
 from torch.optim import Adam
 
+from src.models.components.inverse_dynamics.inverse_dynamics import InverseDynamics
 from src.models.components.inverse_dynamics.inverse_dynamics_lit_module import (
     InverseDynamicsLitModule as cls,
 )
 
-from ..dummy.inverse_dynamics.dummy_inverse_dynamics import DummyInverseDynamics
-
-in_dim = 64
-out_dim = 3
-
-
-def make_batch(num_batch: int):
-    batch = (
-        torch.randn((num_batch, out_dim), requires_grad=True),
-        torch.randn((num_batch, in_dim), requires_grad=True),
-        torch.randn((num_batch, out_dim), requires_grad=True),
-        torch.randn((num_batch, in_dim), requires_grad=True),
-    )
-    return batch
-
-
-params = [[make_batch(1), 0], [make_batch(8), 1]]
-
 
 class TestInverseDynamicsLitModule:
-    @pytest.fixture
-    def dummy_net(self):
-        return DummyInverseDynamics(in_dim, out_dim)
+    batch_size = 8
+    in_shape = (batch_size, 3, 256, 256)
+    out_shape = (batch_size, 3)
 
     @pytest.fixture
-    def optimizer(self, dummy_net):
+    def batch(self):
+        batch = (
+            torch.randn(self.out_shape, requires_grad=True),
+            torch.randn(self.in_shape, requires_grad=True),
+            torch.randn(self.out_shape, requires_grad=True),
+            torch.randn(self.in_shape, requires_grad=True),
+        )
+        return batch
 
-        return Adam(dummy_net.parameters())
+    @pytest.fixture
+    def dummy_net(self, mocker):
+        dummy = mocker.Mock(
+            spec=InverseDynamics, return_value=(torch.empty(self.out_shape), torch.empty(self.out_shape))
+        )
+        return dummy
+
+    @pytest.fixture
+    def optimizer(self):
+        return partial(Adam)
 
     def test__init__(self, dummy_net, optimizer):
         mod = cls(dummy_net, optimizer)
-        assert mod.hparams.inverse_dynamics_net is dummy_net
+        assert mod.net is dummy_net
         assert mod.hparams.optimizer is optimizer
 
-    def test_configure_optimizers(self, dummy_net, optimizer):
+    def test_training_step(self, dummy_net, optimizer, batch):
         mod = cls(dummy_net, optimizer)
-        assert mod.configure_optimizers() is optimizer
+        assert type(mod(batch, 0) == torch.Tensor)
 
-    @pytest.mark.parametrize("batch, batch_idx", params)
-    def test_training_step(self, dummy_net, optimizer, batch, batch_idx):
+    def test_forward(self, dummy_net, optimizer, batch):
         mod = cls(dummy_net, optimizer)
-        init_parameters = [param.clone() for param in mod.hparams.inverse_dynamics_net.parameters()]
-        loss = mod.training_step(batch, batch_idx)
-        loss.backward()
-        mod.hparams.optimizer.step()
-        after_parameters = [param.clone() for param in mod.hparams.inverse_dynamics_net.parameters()]
-
-        # モデルパラメータの更新前後を比較
-        for init_param, after_param in zip(init_parameters, after_parameters):
-            assert not torch.all(
-                torch.eq(
-                    init_param,
-                    after_param,
-                )
-            ), "Model parameters were not updated."
+        _, obs, _, next_obs = batch
+        output = mod.forward(obs, next_obs)
+        assert len(output) == 2
+        for out in output:
+            assert out.shape == torch.Size(self.out_shape)
