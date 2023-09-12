91c9a67d1d27ef2574dcdb65163d122182438b59
myxyy <m.myxomatosis@gmail.com>
2023-09-12 22:43:40 +0900

    ForwardDynamicsLitModuleテストのクラス化

--- tests/models/components/forward_dynamics/test_forward_dynamics_lit_module.py
+++ tests/models/components/forward_dynamics/test_forward_dynamics_lit_module.py
@@ -13,42 +13,40 @@ from src.models.components.observation_encoder.observation_encoder import (
     ObservationEncoder,
 )
 
-num_batch = 32
-obs_emb_dim = 256
-action_dim = 128
-batch = (
-    torch.randn(num_batch, action_dim),
-    torch.randn(num_batch, obs_emb_dim),
-    torch.randn(num_batch, action_dim),
-    torch.randn(num_batch, obs_emb_dim),
-)
 
+class TestForwardDynamicsLitModule:
+    num_batch = 32
+    obs_emb_dim = 256
+    action_dim = 128
+    batch = (
+        torch.randn(num_batch, action_dim),
+        torch.randn(num_batch, obs_emb_dim),
+        torch.randn(num_batch, action_dim),
+        torch.randn(num_batch, obs_emb_dim),
+    )
 
-@pytest.fixture
-def mock_observation_encoder(mocker: MockerFixture) -> ObservationEncoder:
-    mock = mocker.Mock(spec=ObservationEncoder, return_value=torch.randn(num_batch, obs_emb_dim))
-    return mock
+    @pytest.fixture
+    def mock_observation_encoder(self, mocker: MockerFixture) -> ObservationEncoder:
+        mock = mocker.Mock(spec=ObservationEncoder, return_value=torch.randn(self.num_batch, self.obs_emb_dim))
+        return mock
 
+    @pytest.fixture
+    def mock_forward_dynamics_net(self, mocker: MockerFixture) -> ForwardDynamics:
+        mock = mocker.Mock(spec=ForwardDynamics, return_value=torch.randn(self.num_batch, self.obs_emb_dim))
+        return mock
 
-@pytest.fixture
-def mock_forward_dynamics_net(mocker: MockerFixture) -> ForwardDynamics:
-    mock = mocker.Mock(spec=ForwardDynamics, return_value=torch.randn(num_batch, obs_emb_dim))
-    return mock
+    @pytest.fixture
+    def optimizer(self) -> partial[Optimizer]:
+        return partial(Adam)
 
+    @pytest.fixture
+    def forward_dynamics_lit_module(
+        self,
+        mock_observation_encoder: ObservationEncoder,
+        mock_forward_dynamics_net: ForwardDynamics,
+        optimizer: Optimizer,
+    ) -> ForwardDynamicsLitModule:
+        return ForwardDynamicsLitModule(mock_observation_encoder, mock_forward_dynamics_net, optimizer)
 
-@pytest.fixture
-def optimizer() -> partial[Optimizer]:
-    return partial(Adam)
-
-
-@pytest.fixture
-def forward_dynamics_lit_module(
-    mock_observation_encoder: ObservationEncoder,
-    mock_forward_dynamics_net: ForwardDynamics,
-    optimizer: Optimizer,
-) -> ForwardDynamicsLitModule:
-    return ForwardDynamicsLitModule(mock_observation_encoder, mock_forward_dynamics_net, optimizer)
-
-
-def test_forward_dynamics_lit_module(forward_dynamics_lit_module: ForwardDynamicsLitModule):
-    loss = forward_dynamics_lit_module.training_step(batch, 0)
+    def test_forward_dynamics_lit_module(self, forward_dynamics_lit_module: ForwardDynamicsLitModule):
+        loss = forward_dynamics_lit_module.training_step(self.batch, 0)
