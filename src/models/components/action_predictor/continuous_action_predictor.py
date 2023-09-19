b4c9c510e9a99601c3726629f2e6b3d95026639f
cehl-kurage <yusakuishihira@gmail.com>
2023-09-19 21:14:17 +0900

    LargeScaleCuriosityのデフォルト値を反映

--- src/models/components/action_predictor/continuous_action_predictor.py
+++ src/models/components/action_predictor/continuous_action_predictor.py
@@ -6,13 +6,26 @@ from .action_predictor import ActionPredictor
 
 
 class ContinuousActionPredictor(ActionPredictor):
-    def __init__(self, dim_embed: int, dim_action: int):
+    def __init__(self, dim_embed: int, dim_action: int, dim_hidden: int = None):
+        """
+
+        Args:
+            dim_embed (int): _description_
+            dim_action (int): _description_
+            dim_hidden (int, optional): _description_. Defaults to None.
+                                        See https://github.com/openai/large-scale-curiosity/blob/master/run.py#L51
+                                        and https://github.com/openai/large-scale-curiosity/blob/master/auxiliary_tasks.py#L66
+        """
+        if dim_hidden is None:
+            dim_hidden = dim_embed  # 2 * dim_embed -> dim_embed
         super().__init__()
         # 要調整
-        self.fc_in = nn.Linear(dim_embed * 2, dim_embed * 2 + dim_action * 2)
-        self.fc_1 = nn.Linear(dim_embed * 2 + dim_action * 2, dim_embed * 2 + dim_action * 2)
-        self.fc_out = nn.Linear(dim_embed * 2 + dim_action * 2, dim_action * 2)
-        self.relu = nn.ReLU()
+        self.fc_in = nn.Linear(dim_embed * 2, dim_hidden)
+        self.fc_1 = nn.Linear(dim_hidden, dim_hidden)
+        self.fc_out = nn.Linear(dim_hidden, dim_action * 2)
+        self.relu = (
+            nn.ReLU()
+        )  # https://github.com/openai/large-scale-curiosity/blob/master/auxiliary_tasks.py#L66 and https://github.com/openai/large-scale-curiosity/blob/master/utils.py#L115
 
     def forward(self, embed: Tensor, next_embed: Tensor) -> Tensor:
         x = torch.concat([embed, next_embed], dim=-1)
