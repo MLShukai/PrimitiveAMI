e83f482a334c39fee6177356d54f6e5b15217efc
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2023-09-20 16:57:31 +0900

    ADD get_sleep_action func

--- src/environment/actuators/locomotion_actuator.py
+++ src/environment/actuators/locomotion_actuator.py
@@ -1,5 +1,8 @@
 import torch
-from vrchat_io.controller.wrappers.osc import AxesLocomotionWrapper
+from vrchat_io.controller.wrappers.osc import (
+    AXES_LOCOMOTION_RESET_VALUES,
+    AxesLocomotionWrapper,
+)
 
 from .actuator import Actuator
 
@@ -11,3 +14,12 @@ class LocomotionActuator(Actuator):
     def operate(self, action: torch.Tensor) -> None:
         _action = action.detach().cpu().numpy().tolist()
         self.actuator.command(_action[0], _action[1], _action[2])
+
+
+def get_sleep_action() -> torch.Tensor:
+    """Return sleep action for LocomotionActuator.
+
+    Retruns:
+        torch.Tensor: sleep action.
+    """
+    return torch.tensor(AXES_LOCOMOTION_RESET_VALUES, dtype=torch.float).clone()
