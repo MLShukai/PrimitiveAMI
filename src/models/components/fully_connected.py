10e29f4f7765a6308002c7a0a2dddaa85367f684
myxyy <m.myxomatosis@gmail.com>
2023-09-24 12:57:37 +0900

    activationのデフォルトをNoneに変更

--- src/models/components/fully_connected.py
+++ src/models/components/fully_connected.py
@@ -3,12 +3,13 @@ import torch.nn as nn
 
 
 class FullyConnected(nn.Module):
-    def __init__(self, dim_in: int, dim_out: int, activation: nn.Module = nn.ReLU()):
+    def __init__(self, dim_in: int, dim_out: int, activation: nn.Module = None):
         super().__init__()
         self.fc = nn.Linear(dim_in, dim_out)
         self.activation = activation
 
     def forward(self, x):
         x = self.fc(x)
-        x = self.activation(x)
+        if self.activation is not None:
+            x = self.activation(x)
         return x
--- tests/models/components/test_fully_connected.py
+++ tests/models/components/test_fully_connected.py
@@ -1,5 +1,6 @@
 import pytest
 import torch
+import torch.nn as nn
 
 from src.models.components.fully_connected import FullyConnected
 
@@ -9,15 +10,16 @@ from src.models.components.fully_connected import FullyConnected
     batch,
     dim_in,
     dim_out,
+    activation,
     """,
     [
-        (4, 123, 345),
-        (32, 234, 345),
-        (17, 234, 241),
+        (4, 123, 345, None),
+        (32, 234, 345, nn.ReLU()),
+        (17, 234, 241, nn.ReLU()),
     ],
 )
-def test_fully_connected(batch, dim_in, dim_out):
-    fc = FullyConnected(dim_in, dim_out)
+def test_fully_connected(batch, dim_in, dim_out, activation):
+    fc = FullyConnected(dim_in, dim_out, activation=activation)
     x = torch.randn(batch, dim_in)
     x = fc(x)
 
