b9ac017f5eecbac4a4b985bc765fca593fb65211
myxyy <46291391+myxyy@users.noreply.github.com>
2023-09-24 11:29:34 +0900

    Update src/models/components/forward_dynamics/resnet_forward_dynamics.py
    Co-authored-by: GesonAnko <59220704+Geson-anko@users.noreply.github.com>

--- src/models/components/forward_dynamics/resnet_forward_dynamics.py
+++ src/models/components/forward_dynamics/resnet_forward_dynamics.py
@@ -11,18 +11,23 @@ class ResNetForwardDynamics(ForwardDynamics):
         super().__init__()
         self.depth = depth
         self.fc_in = nn.Linear(dim_action + dim_embed + dim_action, dim_hidden)
-        self.fc1_list = nn.ModuleList([nn.Linear(dim_hidden, dim_hidden) for _ in range(depth)])
-        self.fc2_list = nn.ModuleList([nn.Linear(dim_hidden, dim_hidden) for _ in range(depth)])
-        self.fc_out = nn.Linear(dim_hidden, dim_embed)
+        dim_hidden_cat_action = dim_action + dim_hidden + dim_action
+        self.fc1_list = nn.ModuleList([nn.Linear(dim_hidden_cat_action, dim_hidden) for _ in range(depth)])
+        self.fc2_list = nn.ModuleList([nn.Linear(dim_hidden_cat_action, dim_hidden) for _ in range(depth)])
+        self.fc_out = nn.Linear(dim_hidden_cat_action, dim_embed)
         self.leaky_relu = nn.LeakyReLU()
 
     def forward(self, prev_action: Tensor, embed: Tensor, action: Tensor) -> Tensor:
-        x = torch.concat([prev_action, embed, action], dim=-1)
+        x = self.add_action(prev_action, embed, action)
         x = self.fc_in(x)
         for i in range(self.depth):
-            x_ = self.fc1_list[i](x)
+            x_ = self.fc1_list[i](self.add_action(prev_action, x, action))
             x_ = self.leaky_relu(x_)
-            x_ = self.fc2_list[i](x_)
+            x_ = self.fc2_list[i](self.add_action(prev_action, x_, action))
             x = x_ + x
-        next_embed = self.fc_out(x)
+        next_embed = self.fc_out(self.add_action(prev_action, x, action))
         return next_embed
+    
+    @staticmethod
+    def add_action(prev_action, x, action):
+        return torch.concat([prev_action, x, action], dim=-1)
