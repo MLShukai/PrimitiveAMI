aa35572e605d7e7b85136039507d5d95b1868004
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2023-09-28 10:35:10 +0900

    ADD type annotation of data collector

--- src/trainers/ppo_trainer.py
+++ src/trainers/ppo_trainer.py
@@ -1,7 +1,11 @@
+from ..data_collectors.trajectory_data_collector import TrajectoryDataCollector
 from .simple_pl_trainer import SimplePLTrainer
 
 
 class PPOTrainer(SimplePLTrainer):
+
+    data_collector: TrajectoryDataCollector
+
     def train(self):
         super().train()
         self.data_collector.clear()
