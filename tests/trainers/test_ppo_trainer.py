6dd9c728e51257a36877fc0f5e4d352926e164f3
cehl-kurage <yusakuishihira@gmail.com>
2023-09-30 14:29:14 +0900

    ppoにもlogのテストを追加

--- tests/trainers/test_ppo_trainer.py
+++ tests/trainers/test_ppo_trainer.py
@@ -73,10 +73,15 @@ class TestPPOTrainer:
         return partial(DataLoader)
 
     @pytest.fixture
-    def pl_trainer(self) -> pl.Trainer:
+    def logger(self, tmp_path):
+        return pl.loggers.TensorBoardLogger(tmp_path / "tensorboard", name=None, version="")
+
+    @pytest.fixture
+    def pl_trainer(self, logger) -> pl.Trainer:
         return pl.Trainer(
-            max_epochs=1,
-            logger=False,
+            max_steps=10,
+            logger=logger,
+            log_every_n_steps=1,
             enable_checkpointing=False,
             enable_progress_bar=False,
             enable_model_summary=False,
