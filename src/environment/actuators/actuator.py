ff6598baab2c9342b37be102c3a1c39e816da304
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2023-10-01 10:31:30 +0900

    ADD setup and teardown method

--- src/environment/actuators/actuator.py
+++ src/environment/actuators/actuator.py
@@ -6,3 +6,11 @@ class Actuator(metaclass=ABCMeta):
     @abstractmethod
     def operate(action: Any) -> None:
         raise NotImplementedError
+
+    def setup(self):
+        """Called at the start of interaction with the agent."""
+        pass
+
+    def teardown(self):
+        """Called at the end of interaction with the agent."""
+        pass
--- src/environment/sensors/sensor.py
+++ src/environment/sensors/sensor.py
@@ -6,3 +6,11 @@ class Sensor(metaclass=ABCMeta):
     def read(self):
         """Read and return data from the actual sensor."""
         raise NotImplementedError
+
+    def setup(self):
+        """Called at the start of interaction with the agent."""
+        pass
+
+    def teardown(self):
+        """Called at the end of interaction with the agent."""
+        pass
