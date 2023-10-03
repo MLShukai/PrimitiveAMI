0f3d636f2706f174a56d32ced63d72a449979d24
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2023-10-03 16:30:08 +0900

    FIX type annotation

--- src/trainers/builders/trainers_builder.py
+++ src/trainers/builders/trainers_builder.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 
-from ...data_collectors.data_collector import DataCollector
+from ...data_collectors.aggregations.data_collectors_aggregation import (
+    DataCollectorsAggregation,
+)
 from ...models.aggregations.neural_networks import NeuralNetworks
 from ..trainer import Trainer
 
@@ -9,12 +11,12 @@ class TrainersBuilder(ABC):
     """Abstract interface class for building trainers."""
 
     @abstractmethod
-    def build(self, nets: NeuralNetworks, data_collectors: DataCollector) -> Trainer:
+    def build(self, nets: NeuralNetworks, data_collectors: DataCollectorsAggregation) -> Trainer:
         """Build trainers with neural networks and data collectors.
 
         Args:
             nets (NeuralNetworks): Aggregation class of NeuralNetwork models.
-            data_collectors (DataCollector): data_collector(s) class for providing dataset to each trainers.
+            data_collectors (DataCollectorsAggregation): data_collector(s) class for providing dataset to each trainers.
 
         Returns:
             Trainer: Built single trainer class. (Aggregated other trainers.)
