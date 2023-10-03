50a9fbbf0ff8664b913b0f463bceee4ca5af8ed1
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2023-10-03 11:40:06 +0900

    FIX dict data collector match to aggregation class

--- tests/dara_collectors/aggregations/test_dict_data_collectors.py
+++ tests/dara_collectors/aggregations/test_dict_data_collectors.py
@@ -1,23 +1,19 @@
+import os
+from pathlib import Path
 from typing import Any
 
 import pytest
-from pytest_mock import MockerFixture
 
 from src.data_collectors.aggregations.dict_data_collectors import DictDataCollectors
-from src.data_collectors.data_collector import DataCollector
+from src.data_collectors.empty_data_collector import EmptyDataCollector
 
 
 class TestDictDataCollectors:
-    def get_mock_data_collector(self, mocker: MockerFixture, data: Any) -> DataCollector:
-        collector = mocker.Mock(spec=DataCollector)
-        collector.get_data.return_value = data
-        return collector
-
     @pytest.fixture
-    def collectors_dict(self, mocker: MockerFixture) -> dict[str, DataCollector]:
+    def collectors_dict(self) -> dict[str, EmptyDataCollector]:
         return {
-            "collector1": self.get_mock_data_collector(mocker, "data1"),
-            "collector2": self.get_mock_data_collector(mocker, "data2"),
+            "collector1": EmptyDataCollector(),
+            "collector2": EmptyDataCollector(),
         }
 
     @pytest.fixture
@@ -28,14 +24,10 @@ class TestDictDataCollectors:
         assert dict_data_collectors["collector1"] is collectors_dict["collector1"]
         assert dict_data_collectors["collector2"] is collectors_dict["collector2"]
 
-    def test_collect(self, dict_data_collectors: DictDataCollectors, collectors_dict):
+    def test_collect(self, dict_data_collectors: DictDataCollectors):
         dict_data_collectors.collect({})
 
-        for value in collectors_dict.values():
-            value.collect.assert_called_once_with({})
+    def test_save_state_dict_to_files(self, tmp_path: Path, dict_data_collectors: DictDataCollectors):
+        dict_data_collectors.save_state_dict_to_files(tmp_path)
 
-    def test_get_data(self, dict_data_collectors: DictDataCollectors):
-        assert dict_data_collectors.get_data() == {
-            "collector1": "data1",
-            "collector2": "data2",
-        }
+        assert set(os.listdir(tmp_path)) == {"collector1.pkl", "collector2.pkl"}
