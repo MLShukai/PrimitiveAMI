e531005d4373443c141df4f5b47a68976974187c
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2023-10-03 11:51:11 +0900

    FIX sequential data collectors inhertis aggregation class

--- src/data_collectors/aggregations/sequential_data_collectors.py
+++ src/data_collectors/aggregations/sequential_data_collectors.py
@@ -1,9 +1,12 @@
+import warnings
+from pathlib import Path
 from typing import Any
 
 from ..data_collector import DataCollector
+from .data_collectors_aggregation import DataCollectorsAggregation
 
 
-class SequentialDataCollectors:
+class SequentialDataCollectors(DataCollectorsAggregation):
     def __init__(self, *data_collectors: DataCollector) -> None:
         self.data_collectors = data_collectors
 
@@ -12,5 +15,11 @@ class SequentialDataCollectors:
             collector.collect(step_record)
 
     def get_data(self) -> list[Any]:
+        warnings.warn("The `get_data` method ofAggregation class is deprecated!", DeprecationWarning)
         data = [collector.get_data() for collector in self.data_collectors]
         return data
+
+    def save_state_dict_to_files(self, dir_path: Path) -> None:
+        for idx, collector in enumerate(self.data_collectors):
+            dist_path = dir_path / f"{collector.__class__.__name__}.{idx}.pkl"
+            collector.save_to_file(dist_path)
