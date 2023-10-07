b0294123f4201c2bc2b652480b736812b4d1573a
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2023-10-07 17:45:20 +0900

    ADD random agent config

--- tests/config_tests/test_agent.py
+++ tests/config_tests/test_agent.py
@@ -8,3 +8,8 @@ def test_curiosity_ppo_agent(tmp_path):
     cfg = OmegaConf.load(PROJECT_ROOT / "configs/agent/curiosity_ppo_agent.yaml")
     cfg.logger.save_dir = str(tmp_path)
     hydra.utils.instantiate(cfg)
+
+
+def test_random_agent():
+    cfg = OmegaConf.load(PROJECT_ROOT / "configs/agent/random_agent.yaml")
+    hydra.utils.instantiate(cfg)
