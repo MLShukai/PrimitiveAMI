7c40b765cd6b33e588daf391dd19f05c7c1b31e7
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2023-11-07 20:34:38 +0900

    Add VAEForwardPPO trainer configuration

--- tests/config_tests/test_trainers_builder.py
+++ tests/config_tests/test_trainers_builder.py
@@ -18,3 +18,17 @@ def test_inverse_forward_ppo(tmp_path: Path):
         cfg.ppo.pl_trainer.accelerator = "cpu"
 
     hydra.utils.instantiate(cfg)
+
+
+def test_vae_forward_ppo(tmp_path: Path):
+    cfg = OmegaConf.load(PROJECT_ROOT / "configs/trainers_builder/vae_forward_ppo.yaml")
+
+    with open_dict(cfg):
+        cfg.vae_trainer.pl_trainer.default_root_dir = str(tmp_path / "vae")
+        cfg.vae_trainer.pl_trainer.accelerator = "cpu"
+        cfg.forward_dynamics_trainer.pl_trainer.default_root_dir = str(tmp_path / "forward_dynamics")
+        cfg.forward_dynamics_trainer.pl_trainer.accelerator = "cpu"
+        cfg.ppo_trainer.pl_trainer.default_root_dir = str(tmp_path / "ppo")
+        cfg.ppo_trainer.pl_trainer.accelerator = "cpu"
+
+    hydra.utils.instantiate(cfg)
