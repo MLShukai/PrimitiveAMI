14c072a3453787da3847bf00a5992771ae94c40e
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2023-11-29 10:48:35 +0900

    FIX `MultiCategorical` class and test it.

--- src/utils/model.py
+++ src/utils/model.py
@@ -34,21 +34,45 @@ class MultiDistributions(Distribution):
         return [d.entropy() for d in self.dists]
 
 
-class MultiCategoricals(MultiDistributions):
+class MultiCategoricals(Distribution):
     """Set of same action torch.Size categorical distributions."""
 
+    arg_constraints = {}
+
     def __init__(self, distributions: list[Categorical]) -> None:
+        """Constructs Multi Categorical class. All `batch_shape` of child
+        categorical class must be same.
+
+        Args:
+            distributions: A list of Categorical distributions, where each distribution may have a different size of action choices.
+        """
+
         assert len(distributions) > 0
         first_dist = distributions[0]
         assert all(first_dist.batch_shape == d.batch_shape for d in distributions), "All batch shapes must be same."
 
-        super().__init__(distributions)
+        batch_shape = (*first_dist.batch_shape, len(distributions))
+        super().__init__(batch_shape=batch_shape, event_shape=torch.Size(), validate_args=False)
 
-    def sample(self, sample_shape: list[torch.Size] = torch.Size()) -> torch.Tensor:
-        return torch.stack(super().sample(sample_shape))
+        self.dists = distributions
+
+    def sample(self, sample_shape: torch.Size = torch.Size()) -> torch.Tensor:
+        """Sample from each distributions and stacks their outputs.
+
+        Shape:
+            return: (*sample_shape, num_dists)
+        """
+        return torch.stack([d.sample(sample_shape) for d in self.dists], dim=-1)
 
     def log_prob(self, value: torch.Tensor) -> torch.Tensor:
-        return torch.stack(super().log_prob(value))
+        """Compute log probability of `value`
+
+        Shape:
+            value: (*, num_dists)
+            return: (*, num_dists)
+        """
+        return torch.stack([d.log_prob(v) for d, v in zip(self.dists, value.movedim(-1, 0))], dim=-1)
 
     def entropy(self) -> torch.Tensor:
-        return torch.stack(super().entropy())
+        """Compute entropy for each distribution."""
+        return torch.stack([d.entropy() for d in self.dists], dim=-1)
--- tests/utils/test_model.py
+++ tests/utils/test_model.py
@@ -1,9 +1,37 @@
+import pytest
 import torch
 import torch.nn as nn
+from torch.distributions import Categorical
 
-from src.utils.model import SequentialModuleList
+from src.utils.model import MultiCategoricals, SequentialModuleList
 
 
 def test_SequentialModuleList():
     mod = SequentialModuleList([nn.Linear(2, 2) for _ in range(3)])
     mod(torch.randn(2))
+
+
+class TestMultiCategoricals:
+    @pytest.fixture
+    def distributions(self) -> list[Categorical]:
+        choices_per_dist = [3, 2, 5]
+        batch_size = 8
+        return [Categorical(logits=torch.zeros(batch_size, c)) for c in choices_per_dist]
+
+    @pytest.fixture
+    def multi_categoricals(self, distributions) -> MultiCategoricals:
+        return MultiCategoricals(distributions)
+
+    def test_init(self, multi_categoricals: MultiCategoricals):
+        assert multi_categoricals.batch_shape == (8, 3)
+
+    def test_sample(self, multi_categoricals: MultiCategoricals):
+        assert multi_categoricals.sample().shape == (8, 3)
+        assert multi_categoricals.sample((1, 2)).shape == (1, 2, 8, 3)
+
+    def test_log_prob(self, multi_categoricals: MultiCategoricals):
+        sampled = multi_categoricals.sample()
+        assert multi_categoricals.log_prob(sampled).shape == sampled.shape
+
+    def test_entropy(self, multi_categoricals: MultiCategoricals):
+        assert multi_categoricals.entropy().shape == (8, 3)
